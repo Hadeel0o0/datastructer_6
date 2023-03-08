7853102fb5ef3777cde207459b16f1251fe97b9d
Hadeel0o0 <111194916+Hadeel0o0@users.noreply.github.com>
2023-03-09 00:13:38 +0300

    Update q2.java

--- q2.java
+++ q2.java
@@ -1,7 +1,7 @@
 import org.w3c.dom.Node;
 
 public class Q2 {
-    public static void main(String[] args) {
+    public static vo id main(String[] args) {
         singleLinkedList<Integer>list=new singleLinkedList<>();
 
         list.addFirst(11);
